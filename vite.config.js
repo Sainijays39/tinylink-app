e115f077705f26ee08cdbcf52171488f31bf9117
sainijays <sainijays39@gmail.com>
2025-11-22 14:11:23 +0530

    Add vercel.json for React Router routing

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-    open: true,
-    // 👇 This is the key part
-    historyApiFallback: true
-  },
-  // 👇 Add this for production routing on Vercel
-  build: {
-    outDir: 'dist',
-  },
-  preview: {
-    historyApiFallback: true,
-  }
 })
